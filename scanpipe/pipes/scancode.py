e87e3a6fb48aaaba0ca7e39ef7f13d6bb636c63d
Thomas Druez <tdruez@nexb.com>
2021-04-09 16:37:46 +0200

    Add scancode_analyzer in the requirements #30
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

--- scanpipe/pipes/scancode.py
+++ scanpipe/pipes/scancode.py
@@ -140,7 +140,6 @@ def scan_file(location):
     location_rid = location, 0
     _, _, errors, _, results, _ = scancode_cli.scan_resource(location_rid, scanners)
 
-    print("results", results)
     return results, errors
 
 
